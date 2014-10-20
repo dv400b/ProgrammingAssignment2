abe631872fdf2e611d29c517613db6da8c8ba75c
Daniel Kim <dv400b@gmail.com>
2014-10-19 21:26:21 -0400

    Edited for Coursera submission.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+### Functions for manipulating "cached matrix objects," or "CMOs" for short.
 
-## Write a short comment describing this function
-
+#Define methods (embedded functions) that apply to CMOs:
+#  $set:  assign matrix value
+#  $get:  return matrix value
+#  $setInverse:  set inverse matrix value [DO NOT call directly, call cacheSolve() instead] 
+#  $getInverse:  return inverse matrix value
 makeCacheMatrix <- function(x = matrix()) {
-
+   xinv <- NULL
+   set <- function(y) {
+      x    <<- y
+      xinv <<- NULL
+   }
+   get <- function() x
+   setinverse <- function(matr) xinv <<- matr
+   getinverse <- function() xinv
+   list(set = set, get = get,
+        setinverse = setinverse,
+        getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
+## Return the inverse of a CMO.
+# If the CMO contains a cached inverse matrix value, return cached value.
+# Otherwise, calc the inverse matrix, populate the CMO inverse matrix value, and return calced value.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+   xinv <- x$getinverse()
+   if(!is.null(xinv)) {
+      message("getting cached data")
+      return(xinv)
+   }
+   data <- x$get()
+   xinv <- solve(data, ...)
+   x$setinverse(xinv)
+   xinv
 }
